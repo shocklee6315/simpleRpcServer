83004cdb16bbd27c61423ee362393ee47ebdf107
lizd3 <shocklee@vip.qq.com>
2016-07-04 21:41:15 +0800

    哼哼哈嘿

---
+++ src/main/java/com/rpc/test/Po.java
@@ -0,0 +1,40 @@
+package com.rpc.test;
+
+import java.util.Date;
+
+/**
+ * Created by shocklee on 16/7/4.
+ */
+public class Po {
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    int age ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    String name;
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    Date time;
+
+
+}
