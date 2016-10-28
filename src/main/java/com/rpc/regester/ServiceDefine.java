fb0709a20f6630b7221e4da799c79ab9787312e0
lizd3 <shocklee@vip.qq.com>
2016-10-28 23:33:54 +0800

    增加beanfactory定义

---
+++ src/main/java/com/rpc/regester/ServiceDefine.java
@@ -0,0 +1,17 @@
+package com.rpc.regester;
+
+/**
+ * Created by shocklee on 16/6/24.
+ */
+public interface ServiceDefine {
+
+    public String getCode();
+
+
+
+    public String getDefineClass();
+
+    public String getFactoryClassName();
+
+
+}
